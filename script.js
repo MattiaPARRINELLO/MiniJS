102179ecd626de7ff388c46631313fb54c9141f4
Mattia <contactperso.mp@gmail.com>
2022-07-24 14:10:51 +0200

    Cleared project, project finished

--- script.js
+++ script.js
@@ -2,28 +2,30 @@ document.body.style.display = "none";
 
 const url = new URLSearchParams(window.location.search);
 const beta = url.get("beta");
-var datas =""
+var datas = "";
 
 var date = new Date();
 var endDate = new Date("July 1, 2022 12:00:00");
 
 var allow = false;
 
-var json = $.getJSON('../beta.json').done (function () {
-    datas= json.responseJSON
-    if (datas.includes(beta)) {
-      allow = true;
-    } 
-    else if (date > endDate) {
-      allow = false;
-    } 
-},)
+var json = $.getJSON("../beta.json").done(function () {
+  datas = json.responseJSON;
+  if (datas.includes(beta)) {
+    allow = true;
+  } else if (date > endDate) {
+    allow = false;
+  }
+});
 
 setTimeout(() => {
   if (allow) {
     document.body.style.display = "block";
     console.log("Access granted");
-    console.log("Wellcome to the beta version of the website!, if you find any bugs please report them to the developer, this project will be released the" + endDate);
+    console.log(
+      "Welcome to the beta version of the website!, if you find any bugs please report them to the developer, this project will be released the" +
+        endDate
+    );
   } else {
     document.body.style.display = "none";
     console.log("Access denied");
@@ -32,43 +34,47 @@ setTimeout(() => {
 }, 1500);
 
 window.onload = () => {
-    const runAllScripts = () => {
-      initializeAllAccordions()
-    }
+  const runAllScripts = () => {
+    initializeAllAccordions();
+  };
 
-    const listenForUrlChanges = () => {
-      let url = location.href
-      document.body.addEventListener(
-        'click',
-        () => {
-          requestAnimationFrame(() => {
-            if (url !== location.href) {
-              runAllScripts()
-              url = location.href
-            }
-          })
-        },
-        true
-      )
-    }
-
-    const initializeAllAccordions = () => {
-      const allAccordions = document.querySelectorAll('[data-role="Accordion"]');
-
-      allAccordions.forEach((accordion) => {
-        const accordionHeader = accordion.querySelector('[data-type="accordion-header"]')
-        const accordionContent = accordion.querySelector('[data-type="accordion-content"]')
-
-        accordionHeader.addEventListener('click', () => {
-          if (accordionContent.style.maxHeight) {
-            accordionContent.style.maxHeight = ''
-          } else {
-            accordionContent.style.maxHeight = `${accordionContent.scrollHeight}px`
+  const listenForUrlChanges = () => {
+    let url = location.href;
+    document.body.addEventListener(
+      "click",
+      () => {
+        requestAnimationFrame(() => {
+          if (url !== location.href) {
+            runAllScripts();
+            url = location.href;
           }
-        })
-      })
-    }
+        });
+      },
+      true
+    );
+  };
 
-    listenForUrlChanges()
-    runAllScripts()
-  }
+  const initializeAllAccordions = () => {
+    const allAccordions = document.querySelectorAll('[data-role="Accordion"]');
+
+    allAccordions.forEach((accordion) => {
+      const accordionHeader = accordion.querySelector(
+        '[data-type="accordion-header"]'
+      );
+      const accordionContent = accordion.querySelector(
+        '[data-type="accordion-content"]'
+      );
+
+      accordionHeader.addEventListener("click", () => {
+        if (accordionContent.style.maxHeight) {
+          accordionContent.style.maxHeight = "";
+        } else {
+          accordionContent.style.maxHeight = `${accordionContent.scrollHeight}px`;
+        }
+      });
+    });
+  };
+
+  listenForUrlChanges();
+  runAllScripts();
+};
--- soon/script.js
+++ soon/script.js
@@ -2,41 +2,42 @@ var day = document.getElementById("days");
 var hour = document.getElementById("hours");
 var minute = document.getElementById("minutes");
 var second = document.getElementById("seconds");
-var datas =""
-var json = $.getJSON('../beta.json').done (function () {
-    datas= json.responseJSON
-},)
+var datas = "";
+var json = $.getJSON("../beta.json").done(function () {
+  datas = json.responseJSON;
+});
 
 setInterval(updateTimer, 1000);
 
-function updateTimer () {
-var date = new Date();
-var endDate = new Date("July 1, 2022 12:00:00");
+function updateTimer() {
+  var date = new Date();
+  var endDate = new Date("July 1, 3000 12: 00: 00");
 
-var timeLeft = endDate - date;
+  var timeLeft = endDate - date;
 
-var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-day.innerHTML = days;
-hour.innerHTML = hours;
-minute.innerHTML = minutes;
-second.innerHTML = seconds;
+  day.innerHTML = days;
+  hour.innerHTML = hours;
+  minute.innerHTML = minutes;
+  second.innerHTML = seconds;
 }
 
 function button() {
-    //data includes the beta code
-    var input = prompt("Enter your beta code");	
+  //data includes the beta code
+  var input = prompt("Enter your beta code");
 
-    if (input == null) {
-        return;
-    }else if (datas.includes(input)) {
-        window.location.href = "../?beta=" + input;
-    } else {
-        alert("You realy througt you can acces the beta version of the website? YOU CAN'T!😈");
-        // window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    }
+  if (input == null) {
+    return;
+  } else if (datas.includes(input)) {
+    window.location.href = "../?beta=" + input;
+  } else {
+    alert(
+      "You realy through you can access the beta version of the website? YOU CAN'T!😈"
+    );
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }
 }
-
